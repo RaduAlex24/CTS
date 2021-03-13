c3fd5c0fedabf4790408ed03cbc337e097b2370b
Alex <radualexandru18@stud.ase.ro>
2021-03-13 11:26:04 +0200

    Modificare functrie citire aplicant si restul

--- Seminar2/src/ro/ase/cts/clase/Aplicant.java
+++ Seminar2/src/ro/ase/cts/clase/Aplicant.java
@@ -1,66 +1,97 @@
 package ro.ase.cts.clase;
 
-public abstract class Aplicant{
-	protected String nume;
-	protected String prenume;
-	protected int varsta;
-	protected int punctaj;
-	protected int nr_proiecte;
-	protected String[] denumireProiect;
-	
-	
-	public String getNume() {
-		return nume;
-	}
-	public void setNume(String nume) {
-		this.nume = nume;
-	}
-	public String getPrenume() {
-		return prenume;
-	}
-	public void setPrenume(String prenume) {
-		this.prenume = prenume;
-	}
-	public int getVarsta() {
-		return varsta;
-	}
-	public void setVarsta(int varsta) {
-		this.varsta = varsta;
-	}
-	public void statut(){
-		if(punctaj>80)
-			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
-		else
-			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
-		}
-	public int getPunctaj() {
-		return punctaj;
-	}
-	public void setPunctaj(int punctaj) {
-		this.punctaj = punctaj;
-	}
-	
-	
+import java.util.Arrays;
 
-	
-	public Aplicant() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect) {
-		super();
-		this.nume = nume;
-		this.prenume = prenume;
-		this.varsta = varsta;
-		this.punctaj = punctaj;
-		this.nr_proiecte = nr_proiecte;
-		this.denumireProiect = denumireProiect;
-	}
-	public int getNr_proiecte() {
-		return nr_proiecte;
-	}
-	public void setNr_proiecte(int nr_proiecte) {
-		this.nr_proiecte = nr_proiecte;
-	}
+public abstract class Aplicant {
+    protected String nume;
+    protected String prenume;
+    protected int varsta;
+    protected int punctaj;
+    protected int nr_proiecte;
+    protected String[] denumireProiect;
+
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+    // Trebuie modificat "80" si se repeta system out
+    public void statut() {
+        if (punctaj > 80)
+            System.out.println("Aplicantul " + nume + " " + prenume + " a fost acceptat.");
+        else
+            System.out.println("Aplicantul " + nume + " " + prenume + " nu a fost acceptat.");
+    }
+
+    public int getPunctaj() {
+        return punctaj;
+    }
+
+    public void setPunctaj(int punctaj) {
+        this.punctaj = punctaj;
+    }
+
+
+    public Aplicant() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Aplicant(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect) {
+        super();
+        this.nume = nume;
+        this.prenume = prenume;
+        this.varsta = varsta;
+        this.punctaj = punctaj;
+        this.nr_proiecte = nr_proiecte;
+        this.denumireProiect = denumireProiect;
+    }
+
+    public int getNr_proiecte() {
+        return nr_proiecte;
+    }
+
+    public String[] getDenumireProiect() {
+        return denumireProiect;
+    }
+
+    public void setDenumiProiecte(String[] denumireProiect, int nr_proiecte) {
+        this.denumireProiect = denumireProiect;
+    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Aplicant{");
+        sb.append("nume='").append(nume).append('\'');
+        sb.append(", prenume='").append(prenume).append('\'');
+        sb.append(", varsta=").append(varsta);
+        sb.append(", punctaj=").append(punctaj);
+        sb.append(", nr_proiecte=").append(nr_proiecte);
+        sb.append(", denumireProiect=").append(Arrays.toString(denumireProiect));
+        sb.append('}');
+        return sb.toString();
+    }
+
 
 }
--- Seminar2/src/ro/ase/cts/reader/ReaderStudent.java
+++ Seminar2/src/ro/ase/cts/reader/ReaderStudent.java
@@ -43,18 +43,23 @@ public class ReaderStudent extends ReaderAplicant {
         List<Aplicant> studenti = new ArrayList<Aplicant>();
 
         while (input.hasNext()) {
-            String nume = input.next();
-            String prenume = (input.next()).toString();
-            int varsta = Integer.valueOf(input.nextInt());
-            int punctaj = Integer.valueOf(input.nextInt());
-            int nr = Integer.valueOf(input.nextInt());
-            String[] vect = new String[5];
-            for (int i = 0; i < nr; i++)
-                vect[i] = input.next();
+//            String nume = input.next();
+//            String prenume = (input.next()).toString();
+//            int varsta = Integer.valueOf(input.nextInt());
+//            int punctaj = Integer.valueOf(input.nextInt());
+//            int nr = Integer.valueOf(input.nextInt());
+//            String[] vect = new String[5];
+//            for (int i = 0; i < nr; i++)
+//                vect[i] = input.next();
+            Student student = new Student();
+            super.readAplicant(input, student);
+
             int an_studii = input.nextInt();
             String facultate = (input.next()).toString();
-            Student s = new Student(nume, prenume, varsta, punctaj, nr, vect, facultate, an_studii);
-            studenti.add(s);
+            //Student s = new Student(nume, prenume, varsta, punctaj, nr, vect, facultate, an_studii);
+            student.setFacultate(facultate);
+            student.setAn_studii(an_studii);
+            studenti.add(student);
         }
         input.close();
         return studenti;
