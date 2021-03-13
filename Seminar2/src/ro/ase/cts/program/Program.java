1e1a9a8afd0fde561cee5956eee884fc167fce73
Alex <radualexandru18@stud.ase.ro>
2021-03-13 09:58:21 +0200

    Modificare interfanta in clasa si incapsulare nume fisier

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,8 +1,6 @@
 package ro.ase.cts.program;
 
-import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
-import ro.ase.cts.reader.ReaderAngajati;
 import ro.ase.cts.reader.ReaderAplicant;
 import ro.ase.cts.reader.ReaderStudent;
 
@@ -11,20 +9,20 @@ import java.util.List;
 
 public class Program {
 
-	public static List<Aplicant> citireAplicanti(ReaderAplicant readerAplicant, String numeFisier) throws FileNotFoundException {
-		return readerAplicant.readAplicants(numeFisier);
-	}
+    public static List<Aplicant> citireAplicanti(ReaderAplicant readerAplicant) throws FileNotFoundException {
+        return readerAplicant.readAplicants();
+    }
 
-	public static void main(String[] args) {
-		List<Aplicant> listaAplicanti;
-		try {
-			listaAplicanti = citireAplicanti(new ReaderStudent(), "studenti.txt");
-			for(Aplicant aplicant:listaAplicanti)
-				System.out.println(aplicant.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        List<Aplicant> listaAplicanti;
+        try {
+            listaAplicanti = citireAplicanti(new ReaderStudent("studenti.txt"));
+            for (Aplicant aplicant : listaAplicanti)
+                System.out.println(aplicant.toString());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
