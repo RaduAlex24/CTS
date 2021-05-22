764b323dc985ffa0070c7ac80805105b0d7f56a2
Alex <radualexandru18@stud.ase.ro>
2021-05-22 11:20:11 +0300

    Junit tests final

--- Seminar12/src/tests/StudentTests.java
+++ Seminar12/src/tests/StudentTests.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class StudentTests {
 
     @Test
-    public void testCorectitudineConstructorCuParametru(){
+    public void testCorectitudineConstructorCuParametru() {
         String nume = "Alex";
         Student student = new Student(nume);
 
@@ -16,14 +16,14 @@ public class StudentTests {
     }
 
     @Test
-    public void testInitializareListaInconstructor(){
+    public void testInitializareListaInconstructor() {
         Student student = new Student("Alex");
 
         assertNotNull(student.getNote());
     }
 
     @Test
-    public void testCorectitudineConstructorFaraParametri(){
+    public void testCorectitudineConstructorFaraParametri() {
         Student student = new Student();
 
         assertNotNull("Numele nu a fost initializat", student.getNume());
@@ -31,13 +31,73 @@ public class StudentTests {
     }
 
     @Test
-    public void testMetodaAdaugareNotaInLista(){
+    public void testMetodaAdaugareNotaInLista() {
         int nota = 10;
         Student student = new Student();
         student.adaugaNota(nota);
 
         assertEquals(student.getNota(0), nota);
         assertEquals(student.getNote().size(), 1);
+    }
+
+    @Test
+    public void testDimensiuneLista() {
+        Student student = new Student();
+        student.adaugaNota(10);
+        student.adaugaNota(9);
+        assertEquals(2, student.getNote().size());
+    }
+
+    @Test
+    public void testCalculeazaMediaCorecta() {
+        Student student = new Student();
+        student.adaugaNota(10);
+        student.adaugaNota(9);
+        assertEquals(9.5f, student.calculeazaMedie(), 0.01);
+    }
+
+    @Test
+    public void testStudentCuRestanta(){
+        Student student = new Student();
+        student.adaugaNota(4);
+        assertTrue(student.areRestante());
+    }
+
+    @Test
+    public void testStudentFaraRestanta(){
+        Student student = new Student();
+        student.adaugaNota(10);
+        assertFalse(student.areRestante());
+    }
+
+    @Test
+    public void testGetNotaIndexInvalid(){
+        Student student = new Student();
+        student.adaugaNota(10);
+//1
+        try{
+            //2
+            int nota = student.getNota(-1);
+            //3-nu se ajunge
+            fail("Nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+        }
+        catch (IndexOutOfBoundsException exception){
+            //4
+        }
+        catch (Exception exception){
+            //5-nu se ajunge
+            fail("Tipul exceptiei aruncate nu este corect");
+        }
+        //6
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNotaIndexInvalidJunit4(){
+        Student student = new Student();
+        student.adaugaNota(10);
+
+        int nota = student.getNota(-1);
     }
 
 }
